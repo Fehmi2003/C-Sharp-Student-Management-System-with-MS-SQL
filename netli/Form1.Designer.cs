fec093218262a8e8575984feca44d633d120bda7
FehmiTD <fehmitahsindemirkan@gmail.com>
2023-11-28 15:55:44 +0300

    Add project files.

---
+++ netli/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace netli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtSehir = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnEkle.Location = new System.Drawing.Point(251, 160);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(104, 29);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnSil.Location = new System.Drawing.Point(371, 160);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(104, 29);
+            this.btnSil.TabIndex = 1;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnGuncelle.Location = new System.Drawing.Point(504, 160);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(104, 29);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(267, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ögrenci No";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Öğrenci İsim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Öğrenci Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(267, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Öğrenci Şehir";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(371, 21);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(143, 22);
+            this.txtNo.TabIndex = 7;
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(371, 50);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(143, 22);
+            this.txtIsim.TabIndex = 8;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(371, 85);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(143, 22);
+            this.txtSoyad.TabIndex = 9;
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.Location = new System.Drawing.Point(371, 125);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(143, 22);
+            this.txtSehir.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(96, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(697, 295);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 517);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtIsim);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtSehir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ netli/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace netli
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        void OgrenciGetir()
+        { 
+           
+            con = new SqlConnection("server=.;Initial Catalog=FEHMİTD;Integrated Security=SSPI");
+            con.Open();
+            da = new SqlDataAdapter("SELECT * FROM ogrenciler", con);
+            DataTable tablo =new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        OgrenciGetir();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)//DATAGRİDVİEW GÖRÜNTÜLEME KISMI
+        {
+            txtNo.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtIsim.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtSehir.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtSoyad.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)//EKLEME BUTONU
+        {
+            string sorgu = "INSERT INTO ogrenciler(ogr_no,ogr_ad,ogr_soyad,ogr_sehir)VALUES(@ogr_no,@ogr_ad,@ogr_soyad,@ogr_sehir)";
+            cmd=new SqlCommand(sorgu,con);
+            cmd.Parameters.AddWithValue("@ogr_no", txtNo.Text);
+            cmd.Parameters.AddWithValue("@ogr_ad", txtIsim.Text);
+            cmd.Parameters.AddWithValue("@ogr_soyad", txtSoyad.Text);
+            cmd.Parameters.AddWithValue("@ogr_sehir", txtSehir.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            OgrenciGetir();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)//SİLME BUTONU
+        {
+            String sorgu = "DELETE FROM ogrenciler WHERE ogr_no=@ogr_no";
+            cmd = new SqlCommand(sorgu,con);
+            cmd.Parameters.AddWithValue("@ogr_no",Convert.ToInt32(txtNo.Text));
+            con.Open() ;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            OgrenciGetir();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)//GÜNCELLE BUTONU
+        {
+            String sorgu = "UPDATE ogrenciler SET ogr_ad=@ogr_ad,ogr_soyad=@ogr_soyad,ogr_sehir=@ogr_sehir WHEREO ogr_no=@ogr_no)";
+            cmd=new SqlCommand(sorgu,con);
+            cmd.Parameters.AddWithValue("@ogr_no", txtNo.Text);
+            cmd.Parameters.AddWithValue("@ogr_ad", txtIsim.Text);
+            cmd.Parameters.AddWithValue("@ogr_soyad", txtSoyad.Text);
+            cmd.Parameters.AddWithValue("@ogr_sehir", txtSehir.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            OgrenciGetir();
+        }
+    }
+}
